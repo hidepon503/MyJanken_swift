5aff8e3340c0b8fcb3e5a5e4700634d7498bba0f
hidepon503 <blue.passion625@gmail.com>
2023-12-20 17:57:32 +0900

    Initial Commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by 飯塚 秀幸 on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 飯塚 秀幸 on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
